2064e67ad454f01319c9f13da4407a2005312bfd
Sara-Hosseinabadi <121258586+Sara-Hosseinabadi@users.noreply.github.com>
2023-02-09 13:01:36 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
+  let apiKey = "40b745c14eadad7b7c4e6e4bf3b70103";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unites=metric`;
 
   axios.get(apiUrl).then(displayForecast);
@@ -97,7 +97,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
+  let apiKey = "40b745c14eadad7b7c4e6e4bf3b70103";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
